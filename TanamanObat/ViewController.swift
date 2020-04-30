65b4807863638284c5055006698abd5045fdbf94
zidniryi <zidnirydwan@gmail.com>
2020-04-30 22:19:49 +0700

    Init Project Stricture

---
+++ TanamanObat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TanamanObat
+//
+//  Created by mac on 30/04/20.
+//  Copyright © 2020 zidniryi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
