cbdaed7fe769740e19cbe40a22b05a57312760fa
zidniryi <zidnirydwan@gmail.com>
2020-04-30 22:52:50 +0700

    Make Table View Cell

---
+++ TanamanObat/Controller/PlantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlantTableViewCell.swift
+//  TanamanObat
+//
+//  Created by mac on 30/04/20.
+//  Copyright © 2020 zidniryi. All rights reserved.
+//
+
+import UIKit
+
+class PlantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
