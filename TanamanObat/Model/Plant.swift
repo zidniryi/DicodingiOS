dd21957c27a29f6998cd8a9cc0e3cf52a7b0a296
zidniryi <zidnirydwan@gmail.com>
2020-04-30 22:29:16 +0700

    Make Model For Data

---
+++ TanamanObat/Model/Plant.swift
@@ -0,0 +1,19 @@
+//
+//  Plant.swift
+//  TanamanObat
+//
+//  Created by mac on 30/04/20.
+//  Copyright © 2020 zidniryi. All rights reserved.
+//
+
+import UIKit
+
+struct Tanaman {
+    let image: UIImage
+    let name: String
+    let kingdom: String
+    let division: String
+    let description: String
+    let benefit: String
+}
+
