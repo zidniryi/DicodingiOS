2499eb5979aec2a8e14619c2ac0bf94318de85f5
zidniryi <zidnirydwan@gmail.com>
2020-05-02 04:03:08 +0700

    adding profile

---
+++ TanamanObat/Controller/ProfileViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileViewController.swift
+//  TanamanObat
+//
+//  Created by mac on 02/05/20.
+//  Copyright © 2020 zidniryi. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var orangeView: UIView!
+    @IBOutlet weak var imageProfile: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        orangeView.layer.cornerRadius = 5
+        orangeView.layer.masksToBounds = true
+        
+        imageProfile.layer.cornerRadius = imageProfile.frame.height / 2
+        imageProfile.clipsToBounds = true
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
